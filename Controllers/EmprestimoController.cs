d432a6d0adfeb306ea281aed2cedd40c122c93dc
Wallace Vilela <wallace.vilela9@gmail.com>
2023-12-05 19:35:11 -0300

    Excluir um registro de emprestimo

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -58,5 +58,30 @@ namespace EmprestimoApp.Controllers
             }
             return View(emprestimo);
         }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id){
+            if(id == null || id == 0){
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+            if(emprestimo == null){
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo){
+            if(emprestimo == null){
+                return NotFound();
+            }
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
